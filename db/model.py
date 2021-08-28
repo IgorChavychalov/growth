53dbd4252ec6ccf0bf6400cf14b3bd85779bd949
IgorChavychalov <Protor_fenix@mail.ru>
2021-08-28 14:48:00 +0300

    DB: create plot table

--- db/model.py
+++ db/model.py
@@ -1,6 +1,6 @@
 import os
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import sessionmaker
 
 FOLDER = os.path.dirname(os.path.abspath(__file__))
@@ -10,8 +10,8 @@ Base = declarative_base()
 engine = create_engine(f'sqlite:///{PATH}', echo=False)
 
 
-class Site(Base):
-    __tablename__ = 'site'
+class Sites(Base):
+    __tablename__ = 'sites'
 
     id = Column(Integer, primary_key=True)
     forestry = Column(String)
@@ -33,14 +33,25 @@ class Site(Base):
         return f'{self.forestry}; {self.kvartal}; {self.vydel}; {self.clearcut}; {self.planting}; {self.thining}'
 
 
+class Plots(Base):
+    __tablename__ = 'plots'
 
+    id = Column(Integer, primary_key=True)
+    id_site = Column(Integer, ForeignKey('sites.id'))
+    TLU = Column(String)
+    forest_type = Column(String)
+    number = Column(Integer, nullable=False)
+    area = Column(Integer, nullable=False)
 
+    def __init__(self, site, TLU, forest_type, number, area):
+        self.id_site = site
+        self.TLU = TLU
+        self.forest_type = forest_type
+        self.number = number
+        self.area = area
 
-
-
-
-
-
+    def __repr__(self):
+        return f'{self.id_site}; {self.TLU}; {self.forest_type}; {self.number}; {self.area}'
 
 
 # применим изменения
--- db/tests/test_taxation_db.py
+++ db/tests/test_taxation_db.py
@@ -1,6 +1,6 @@
 from pytest import raises
 import sqlalchemy.orm
-from db.model import Base, Site
+from db.model import Base, Sites, Plots
 
 
 class TestBase:
@@ -10,19 +10,32 @@ class TestBase:
         Base.metadata.create_all(engine)  # импорт структуры БД
         Session = sqlalchemy.orm.sessionmaker(bind=engine)
         session = Session()
-        # Создаём параметры
-        self.session = session
-        # Запись в таблие User для тестов
-        site1 = Site(forestry='Сяськое',
-                     kvartal='184',
-                     vydel='10',
-                     clearcut=2000,
-                     planting=2001,
-                     thining=2010)
-        self.session.add_all([site1])
 
-    def test_exist_data_site_table(self):
-        result = self.session.query(Site).all()[0].__repr__()
+        self.session = session
+        # Запись в таблие Site для тестов
+        site1 = Sites(forestry='Сяськое', kvartal='184', vydel='10', clearcut=2000, planting=2001, thining=2010)
+        site2 = Sites(forestry='Сяськое', kvartal='77', vydel='1', clearcut=2000, planting=2001, thining=2010)
+        self.session.add_all([site1, site2])
+        # Запись в таблие Site для тестов
+        plot1 = Plots(site=1, TLU='В2', forest_type='ЧС', number=1, area=800)
+        plot2 = Plots(site=1, TLU='В2', forest_type='ЧС', number=2, area=800)
+        plot3 = Plots(site=1, TLU='В2', forest_type='ЧС', number=3, area=800)
+        self.session.add_all([plot1, plot2, plot3])
+
+    def test_exist_data_in_sites_table(self):
+        result = self.session.query(Sites).filter(Sites.id == 1).one().__repr__()
         answer = 'Сяськое; 184; 10; 2000; 2001; 2010'
         # Логин уникальный
         assert result == answer
+
+    def test_exist_data_in_plots_table(self):
+        result = self.session.query(Plots).filter(Plots.id == 1).one().__repr__()
+        answer = '1; В2; ЧС; 1; 800'
+        # Логин уникальный
+        assert result == answer
+
+    def test_relation_plot_in_site_table(self):
+        answer = 1
+        plot = self.session.query(Plots).filter(Plots.id == 3).one()
+        result = self.session.query(Sites).filter(Sites.id == plot.id_site).all()[0]
+        assert result.id == answer
